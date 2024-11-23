285eb7c94295bd38104b72dd49198f27eb395faa
XDuskAshes <thegaymeradummy@gmail.com>
2024-11-23 14:10:00 -0500

    Add simple checker for submodule updater
    In order to prevent repeatedly syncing the same commit over and over, I added a check to make sure it doesn't do that.

--- update-submodules.sh
+++ update-submodules.sh
@@ -6,9 +6,16 @@ OLD_COMMIT=$(git rev-parse HEAD)
 echo CURRENT_COMMIT
 git pull origin main
 NEW_COMMIT=$(git rev-parse HEAD)
-cd ..
-git add nvim
-git commit -m "[submodule] Update nvim" -m "Scripted commit sync (update-submodules.sh) for nvim:" -m "    Old commit: ${OLD_COMMIT}" -m "    New commit: ${NEW_COMMIT}" -m "Done automatically with update-submodules.sh on $(date)"
+
+if [ "$OLD_COMMIT" == "$NEW_COMMIT" ]; then
+    echo "Skipping nvim: already latest"
+    echo "Value of OLD_COMMIT: $OLD_COMMIT"
+    echo "Value of NEW_COMMIT: $NEW_COMMIT"
+else
+    cd ..
+    git add nvim
+    git commit -m "[submodule] Update nvim" -m "Scripted commit sync (update-submodules.sh) for nvim:" -m "    Old commit: ${OLD_COMMIT}" -m "    New commit: ${NEW_COMMIT}" -m "Done automatically with update-submodules.sh on $(date)"
+fi
 
 # Push all submodule commits
 git push -u origin main

e03a61b8ba4d50d29d0563141848275edfe9e5ea
XDuskAshes <thegaymeradummy@gmail.com>
2024-11-23 14:06:09 -0500

    Update submodules updater

--- update-submodules.sh
+++ update-submodules.sh
@@ -2,10 +2,13 @@
 
 # nvim
 cd nvim
+OLD_COMMIT=$(git rev-parse HEAD)
+echo CURRENT_COMMIT
 git pull origin main
+NEW_COMMIT=$(git rev-parse HEAD)
 cd ..
 git add nvim
-git commit -m "[submodule] Update nvim" -m "Done automatically with update-submodules.sh on $(date)"
+git commit -m "[submodule] Update nvim" -m "Scripted commit sync (update-submodules.sh) for nvim:" -m "    Old commit: ${OLD_COMMIT}" -m "    New commit: ${NEW_COMMIT}" -m "Done automatically with update-submodules.sh on $(date)"
 
 # Push all submodule commits
 git push -u origin main

41a40ef6279cf2506b468017fc2eef18f120996a
XDuskAshes <thegaymeradummy@gmail.com>
2024-12-06 18:58:46 -0500

    Update update-submodules.sh for hyprconf

--- update-submodules.sh
+++ update-submodules.sh
@@ -2,19 +2,36 @@
 
 # nvim
 cd nvim
-OLD_COMMIT=$(git rev-parse HEAD)
-echo CURRENT_COMMIT
+NV_OLD_COMMIT=$(git rev-parse HEAD)
 git pull origin main
-NEW_COMMIT=$(git rev-parse HEAD)
+NV_NEW_COMMIT=$(git rev-parse HEAD)
 
-if [ "$OLD_COMMIT" == "$NEW_COMMIT" ]; then
+if [ "$NV_OLD_COMMIT" == "$NV_NEW_COMMIT" ]; then
     echo "Skipping nvim: already latest"
-    echo "Value of OLD_COMMIT: $OLD_COMMIT"
-    echo "Value of NEW_COMMIT: $NEW_COMMIT"
+    echo "Value of NV_OLD_COMMIT: $NV_OLD_COMMIT"
+    echo "Value of NV_NEW_COMMIT: $NV_NEW_COMMIT"
+    cd ..
 else
     cd ..
     git add nvim
-    git commit -m "[submodule] Update nvim" -m "Scripted commit sync (update-submodules.sh) for nvim:" -m "    Old commit: ${OLD_COMMIT}" -m "    New commit: ${NEW_COMMIT}" -m "Done automatically with update-submodules.sh on $(date)"
+    git commit -m "[submodule] Update nvim" -m "Scripted commit sync (update-submodules.sh) for nvim:" -m "    Old commit: ${NV_OLD_COMMIT}" -m "    New commit: ${NV_NEW_COMMIT}" -m "Done automatically with update-submodules.sh on $(date)"
+fi
+
+# hyprconf
+cd hyprconf
+HY_OLD_COMMIT=$(git rev-parse HEAD)
+git pull origin main
+HY_NEW_COMMIT=$(git rev-parse HEAD)
+
+if [ "$OLD_COMMIT" == "$NEW_COMMIT" ]; then
+    echo "Skipping hyprconf: already latest"
+    echo "Value of HY_OLD_COMMIT: $HY_OLD_COMMIT"
+    echo "Value of HY_NEW_COMMIT: $HY_NEW_COMMIT"
+    cd ..
+else
+    cd ..
+    git add hyprconf
+    git commit -m "[submodule] Update hyprconf" -m "Scripted commit sync (update-submodules.sh) for hyprconf:" -m "    Old commit: ${HY_OLD_COMMIT}" -m "    New commit: ${HY_NEW_COMMIT}" -m "Done automatically with update-submodules.sh on $(date)"
 fi
 
 # Push all submodule commits

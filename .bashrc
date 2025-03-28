2be9612d8aa08673f7f1552a49496412350b3b13
XDuskAshes <thegaymeradummy@gmail.com>
2025-03-27 21:19:31 -0400

    Update .bashrc and update-submodules

--- .bashrc
+++ .bashrc
@@ -11,7 +11,6 @@ alias ls="ls --color=auto -hsa"
 alias grep="grep --color=auto"
 alias vim="nvim" # I like nvim a lot.
 alias fp="flatpak" # If I ever need flatpak. AKA whenever I need to download/update Bottles or Sober.
-alias pacyay="yay" # This is mostly for the giggles.
 
 eval "$(zoxide init bash)" # For Zoxide. <https://github.com/ajeetdsouza/zoxide>
 eval "$(thefuck --alias)" # https://archlinux.org/packages/extra/any/thefuck/
--- update-submodules.sh
+++ update-submodules.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
-
-# nvim
-cd nvim
-NV_OLD_COMMIT=$(git rev-parse HEAD)
-git pull origin main
-NV_NEW_COMMIT=$(git rev-parse HEAD)
-
-if [ "$NV_OLD_COMMIT" == "$NV_NEW_COMMIT" ]; then
-    echo "Skipping nvim: already latest"
-    echo "Value of NV_OLD_COMMIT: $NV_OLD_COMMIT"
-    echo "Value of NV_NEW_COMMIT: $NV_NEW_COMMIT"
-    cd ..
-else
-    cd ..
-    git add nvim
-    git commit -m "[submodule] Update nvim" -m "Scripted commit sync (update-submodules.sh) for nvim:" -m "    Old commit: ${NV_OLD_COMMIT}" -m "    New commit: ${NV_NEW_COMMIT}" -m "Done automatically with update-submodules.sh on $(date)"
-fi
-
-# Push all submodule commits
-git push -u origin main
+git pull --recurse-submodules && git submodule update --recursive --remote

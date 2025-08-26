2739d62b69376f41a33ef50f481edc0e43eac46a
XDuskAshes <thegaymeradummy@gmail.com>
2025-08-26 01:31:44 -0400

    config.fish: add descriptions and remove prompt
    prompt is moved to functions/fish_prompt.fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,13 @@
     # Commands to run in interactive sessions can go here
 # end
 
+# look and feel
+
+
+# functions, abbreivations, etc etc
 command zoxide init fish | source
 set -gx BROWSER librewolf
+set fish_prompt_pwd_dir_length 4
 
 # stuff that i dont wanna write functions for
 abbr nv nvim
@@ -18,28 +23,32 @@ abbr lg lazygit
 abbr cls clear
 
 # pacman
-function pm ; sudo pacman $argv ; end
-function pmi ; sudo pacman -S $argv ; end
-function pmr ; sudo pacman -R $argv ; end
-function syu ; sudo pacman -Syu ; end
+function pm -d "pacman wrapper" ; sudo pacman $argv ; end
+function pmi -d "pacman -S wrapper" ; sudo pacman -S $argv ; end
+function pmr -d "pacman -R wrapper" ; sudo pacman -R $argv ; end
+function syu -d "pacman -Syu wrapper" ; sudo pacman -Syu ; end
 
 # flatpak
-function fp ; flatpak $argv ; end 
-function fpu ; flatpak update $argv ; end
-function fpi ; flatpak install $argv ; end
-function fpd ; flatpak remove $argv ; end
-function fpr ; flatpak run $argv ; end
+function fp -d "flatpak wrapper" ; flatpak $argv ; end 
+function fpu -d "flatpak update wrapper" ; flatpak update $argv ; end
+function fpi -d "flatpak install wrapper" ; flatpak install $argv ; end
+function fpd -d "flatpak remove wrapper (which i sometimes mistype as flatpak delete)" ; flatpak remove $argv ; end
+function fpr -d "flatpak run wrapper" ; flatpak run $argv ; end
 
 # basically just aliases kind of but more
-function ls ; command ls --color=auto -hsa $argv ;end
-function grep ; command grep --color=auto $argv ; end
+function ls -d "ls wrapper" ; command ls --color=auto -hsa $argv ;end
+function grep -d "grep wrapper" ; command grep --color=auto $argv ; end
 
-# [ ~ ]
-function fish_prompt
-    string join '' -- '[' ' ' (prompt_pwd) ' ' ']' ' '
+# config reload
+function reload -d "Reload the config"
+    source ~/.config/fish/config.fish
+    for file in ~/.config/fish/functions/*.fish
+        source $file
+    end
+    echo Reloaded!
 end
 
 # down down down..
-function .. ; cd .. ; end
-function .... ; cd ../.. ; end
-function ...... ; cd ../../.. ; end
+function .. -d "Down 1 directory" ; cd .. ; end
+function .... -d "Down 2 directories" ; cd ../.. ; end
+function ...... -d "Down 3 directories" ; cd ../../.. ; end

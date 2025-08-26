ce0f288033bb4ffe60be7e89fc199d3d919528dc
XDuskAshes <thegaymeradummy@gmail.com>
2025-08-26 01:32:06 -0400

    fish_prompt.fish in seperate file

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,4 +1,12 @@
-function fish_prompt
-    string join '' -- '[' ' ' (prompt_pwd) ' ' ']' ' '
+# on a zero exit code:
+# working directory ><>
+# otherwise:
+# [exit code] working diretory ><>
+function fish_prompt -d " ~ ><>"
+    set -l last_status $status
+    set -l status_text
+    if test $last_status -ne 0
+        set status_text (set_color red)"[$last_status]"(set_color normal) ' '
+    end
+    string join '' -- $status_text (prompt_pwd) ' ><>' ' '
 end
-

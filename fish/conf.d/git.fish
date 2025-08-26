b5ed463cdac5f8efd093e39b8d7d30062fa51e9e
XDuskAshes <thegaymeradummy@gmail.com>
2025-08-26 01:35:30 -0400

    add descriptions to conf.d/git.fish funcs

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -1,5 +1,5 @@
 # git-specifics
  
-function gp ; git push $argv ; end
-function ga ; git add $argv ; end
-function gc ; git commit $argv ; end
+function gp -d "git push wrapper" ; git push $argv ; end
+function ga -d "git add wrapper" ; git add $argv ; end
+function gc -d "git commit wrapper" ; git commit $argv ; end
